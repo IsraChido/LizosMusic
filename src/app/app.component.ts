42556f8a8b5b3eeaeebf47d2b2b0752a57033fac
Jesús de Israel Jiménez Tostado <israeljimenez@solemti.com>
2025-02-01 22:42:43 -0700

    feat: general project implementations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { NgxSpinnerComponent } from "ngx-spinner";
 
 @Component({
-  imports: [RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    imports: [RouterModule, NgxSpinnerComponent],
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css",
 })
 export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { appRoutes } from "./app.routes";
-import { eventsProviders } from "../data/events/providers/events.providers";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { apiTokenInterceptor } from "@/core/interceptors/api-token.interceptor";
+import { eventsProviders } from "@/data/events/providers/events.providers";
+import { spinnerInterceptor } from "@/core/interceptors/spinner.interceptor";
+import { provideAnimations } from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(appRoutes), eventsProviders],
+    providers: [
+        provideRouter(appRoutes),
+        eventsProviders,
+        provideHttpClient(
+            withInterceptors([spinnerInterceptor, apiTokenInterceptor])
+        ),
+        provideAnimations(),
+    ],
 };
