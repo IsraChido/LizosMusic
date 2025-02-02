8448a7d4d8edcd8cb2af6f1a8f95ad69bc4c6984
Jesús de Israel Jiménez Tostado <israeljimenez@solemti.com>
2025-02-01 22:39:28 -0700

    feat: join pipe

---
+++ src/common/pipes/join-pipe.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: "join",
+})
+export class JoinPipe implements PipeTransform {
+    transform(values: string[], separator: string = ", "): string {
+        return values.filter((v) => v).join(separator);
+    }
+}
